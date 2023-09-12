906db33b808c0800c83103dd58ab1b29075958f6
Jeimyre Gale Perez <jeimyregaleperez@gmail.com>
2023-09-12 20:39:27 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: "Enchanted (Taylor's Version)",
+        path: 'music/song1.mp3',
+        artist: 'Taylor Swift',
+        cover: 'photos/enchanted.png'
+    },
+    {
+        name: "Back to December (Taylor's Version)",
+        path: 'music/song2.mp3',
+        artist: 'Taylor Swift',
+        cover: 'photos/enchanted.png'
+    },
+    {
+        name: "Mean (Taylor's Version)",
+        path: 'music/song3.mp3',
+        artist: 'Taylor Swift',
+        cover: 'photos/enchanted.png'
+    }
+]
